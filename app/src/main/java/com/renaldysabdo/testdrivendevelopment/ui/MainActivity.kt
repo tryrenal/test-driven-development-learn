b0a7a137ed1a9d3f3250ca500e334643c4a701db
renaldy <renalraion@gmail.com>
2020-12-25 21:56:56 +0700

    [add] adding layouting for project

--- app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/MainActivity.kt
+++ app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.renaldysabdo.testdrivendevelopment
+package com.renaldysabdo.testdrivendevelopment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.renaldysabdo.testdrivendevelopment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
