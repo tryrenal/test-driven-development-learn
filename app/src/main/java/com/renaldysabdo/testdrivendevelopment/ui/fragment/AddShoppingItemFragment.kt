4b0546065edc97cc50da19d7ac2242e9d0f66d99
renaldy <renalraion@gmail.com>
2020-12-27 01:11:32 +0700

    [add] adding setup for shoppingFragment and AddShoppingItemFragment

--- app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/AddShoppingItemFragment.kt
+++ app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/AddShoppingItemFragment.kt
@@ -1,17 +1,50 @@
 package com.renaldysabdo.testdrivendevelopment.ui.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.renaldysabdo.testdrivendevelopment.R
+import com.renaldysabdo.testdrivendevelopment.databinding.FragmentAddShoppingItemBinding
 import com.renaldysabdo.testdrivendevelopment.ui.ShoppingViewModel
 
 class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+
     lateinit var shopViewModel : ShoppingViewModel
+
+    private lateinit var binding : FragmentAddShoppingItemBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddShoppingItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         shopViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        binding.ivShoppingImage.setOnClickListener {
+            findNavController().navigate(
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+
+        //karena kita menggunakan satu viewModel maka kita perlu untuk mengeset current image menjadi empty kembali
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                shopViewModel.setCurImageUrl("")
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
 }
--- app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/ShoppingFragment.kt
+++ app/src/main/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/ShoppingFragment.kt
@@ -1,18 +1,33 @@
 package com.renaldysabdo.testdrivendevelopment.ui.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.renaldysabdo.testdrivendevelopment.R
+import com.renaldysabdo.testdrivendevelopment.databinding.FragmentShoppingBinding
 import com.renaldysabdo.testdrivendevelopment.ui.ShoppingViewModel
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
 
     lateinit var shopViewModel : ShoppingViewModel
 
+    private lateinit var binding : FragmentShoppingBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentShoppingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         shopViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        binding.fabAddShoppingItem.setOnClickListener{
+            findNavController().navigate(ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment())
+        }
     }
 }
