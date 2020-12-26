41f4572ede571487a8dee0e50116545091d69db9
renaldy <renalraion@gmail.com>
2020-12-27 01:30:39 +0700

    [add] adding testcase for navigation fragment with click fab button

---
+++ app/src/androidTest/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/ShoppingFragmentTest.kt
@@ -0,0 +1,48 @@
+package com.renaldysabdo.testdrivendevelopment.ui.fragment
+
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.renaldysabdo.testdrivendevelopment.R
+import com.renaldysabdo.testdrivendevelopment.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@ExperimentalCoroutinesApi
+@HiltAndroidTest
+@MediumTest
+class ShoppingFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup(){
+        hiltRule.inject()
+    }
+
+    @Test
+    fun clickAddShoppingItemButton_navigationToAddShoppingItemFragment(){
+        //object memiliki semua fungsi dalam class navController tanpa perlu implemntasi - mock
+        val navController = mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ShoppingFragment> {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+
+        onView(withId(R.id.fabAddShoppingItem)).perform(click())
+
+        verify(navController).navigate(
+            ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+        )
+    }
+}
