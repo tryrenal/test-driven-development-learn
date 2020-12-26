51836835030dac95468ab0a1a1a062fa7ce7985d
renaldy <renalraion@gmail.com>
2020-12-26 14:31:33 +0700

    [add] adding activity for testing fragment with hilt

---
+++ app/src/debug/java/com/renaldysabdo/testdrivendevelopment/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.renaldysabdo.testdrivendevelopment
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+//activity yang digunakan untuk testing
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
