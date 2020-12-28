1bb2419a935ae3d4ddc08cceb8ef808dfd50b17b
renaldy <renalraion@gmail.com>
2020-12-28 09:14:23 +0700

    [add] adding test case for swipe delete item in shopping fragmnet

--- app/src/androidTest/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/renaldysabdo/testdrivendevelopment/ui/fragment/ShoppingFragmentTest.kt
@@ -1,13 +1,22 @@
 package com.renaldysabdo.testdrivendevelopment.ui.fragment
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.renaldysabdo.testdrivendevelopment.R
+import com.renaldysabdo.testdrivendevelopment.adapter.ShoppingItemAdapter
+import com.renaldysabdo.testdrivendevelopment.data.local.ShoppingItem
+import com.renaldysabdo.testdrivendevelopment.getOrAwaitValue
 import com.renaldysabdo.testdrivendevelopment.launchFragmentInHiltContainer
+import com.renaldysabdo.testdrivendevelopment.ui.DefFragmentFactoryTest
+import com.renaldysabdo.testdrivendevelopment.ui.ShoppingViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,6 +25,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 @HiltAndroidTest
@@ -24,6 +34,12 @@ class ShoppingFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactoryTest : DefFragmentFactoryTest
 
     @Before
     fun setup(){
@@ -44,5 +60,24 @@ class ShoppingFragmentTest {
         verify(navController).navigate(
             ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
         )
+    }
+
+    @Test
+    fun swipeShoppingItem_deleteItem(){
+        val shoppingItem = ShoppingItem("name", 1, 1f, "", id = 1)
+        var testViewModel: ShoppingViewModel? = null
+
+        launchFragmentInHiltContainer<ShoppingFragment>(fragmentFactory = fragmentFactoryTest) {
+            testViewModel = shopViewModel
+            shopViewModel?.insertShoppingItemToDb(shoppingItem)
+        }
+
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingItemAdapter.ShoppingItemViewHolder>(
+                0, swipeLeft()
+            )
+        )
+
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
     }
 }
